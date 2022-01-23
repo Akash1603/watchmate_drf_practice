61215fea0a760090de5a0ea40ca19cf16fb3f357
Akash Bawaskar <akashbawaskar786@gmail.com>
2022-01-23 23:05:23 +0530

    C2 Created list endpoint using function base view.

--- watchlist_app/urls.py
+++ watchlist_app/urls.py
@@ -1,3 +1,5 @@
 from django.urls import path
 
-urlpatterns = []
+from watchlist_app.views import movie_list
+
+urlpatterns = [path("movieList/", movie_list, name="movie_list_api")]
--- watchlist_app/views.py
+++ watchlist_app/views.py
@@ -1,3 +1,10 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
+from watchlist_app.models import Movie
+
+
+def movie_list(request):
+    movies = list(Movie.objects.values())
+    return JsonResponse({"Movies": movies}, status=200)
