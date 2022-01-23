8b60691307e952fb469d1ad9e54c191d988c0fd6
Akash Bawaskar <akashbawaskar786@gmail.com>
2022-01-23 23:16:37 +0530

    C3 Created a detail endpoint using function base view and doing manual work.

--- watchlist_app/urls.py
+++ watchlist_app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
-from watchlist_app.views import movie_list
+from watchlist_app.views import movie_list, movie_detail
 
-urlpatterns = [path("movieList/", movie_list, name="movie_list_api")]
+urlpatterns = [
+    path("movieList/", movie_list, name="movie_list_api"),
+    path("movieDetail/<int:pk>/", movie_detail, name="movie_detail_api"),
+]
--- watchlist_app/views.py
+++ watchlist_app/views.py
@@ -8,3 +8,10 @@ from watchlist_app.models import Movie
 def movie_list(request):
     movies = list(Movie.objects.values())
     return JsonResponse({"Movies": movies}, status=200)
+
+
+def movie_detail(request, pk):
+    movie = Movie.objects.get(pk=pk)
+    return JsonResponse({"name": movie.name,
+                         "description": movie.description,
+                         "active": movie.active}, status=200)
